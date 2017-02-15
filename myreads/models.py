d9657f487dc46e007f47b6c81e8a0bf6eb96ad41
TessieWaithira <terrywaithira1@gmail.com>
2017-02-15 22:24:42 +0000

    register models in admin page

--- myreads/models.py
+++ myreads/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+# title
+# text summary
+# date
+# user
+
+class Gist(models.Model):
+    user = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title

96623c71daff18aab211cf60ef5dcea90d9a1460
TessieWaithira <terrywaithira1@gmail.com>
2017-02-16 10:39:43 +0000

    add a gist_detail.html file in the template

--- myreads/urls.py
+++ myreads/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.publish_read, name='publish_read'),
+    url(r'^gist/(?P<pk>\d+)/$', views.gist_detail, name='gist_detail'),
 ]
--- myreads/views.py
+++ myreads/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Gist
 
@@ -7,3 +7,8 @@ from .models import Gist
 def publish_read(request):
     gists = Gist.objects.all()
     return render(request, 'myreads/publish_read.html', context={'gists': gists})
+
+
+def gist_detail(request, pk):
+    gist = get_object_or_404(Gist, pk=pk)
+    return render(request, 'myreads/gist_detail.html', {'gist': gist})
